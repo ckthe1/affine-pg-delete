205ab3d60d8275cc6e455323354eb0af14f11282
Ck T <chengkou@gmail.com>
2019-02-13 13:11:55 -0600

    add post

--- server/public/client.js
+++ server/public/client.js
@@ -3,15 +3,45 @@ $(document).ready(onReady);
 function onReady(){
     console.log('in Jquery');
     $('#submitButton').on('click', addRestaurant);
-
+    addRestaurant();
 
 }; //end onReady
 function addRestaurant(){
-let name = $('#nameIn').val();
-let type = $('#typeIn').val();
-console.log(name,type);
-    $.ajax({ // ajax go to server
+    let name = $('#nameIn').val();
+    let type = $('#typeIn').val();
+    console.log(name,type);
+    $.ajax({
         url: '/restaurant',
-        method: 'GET'//reqst to server           
-    })
+        method: 'POST',
+        data: { 
+            name:name,
+            type: type,
+            
+        }
+    }).then(function (response) {
+        console.log(response);
+
+        getResto();
+    });
+}
+
+
 };//end addRestaurant
+ function getResto(){
+     $.ajax({ // ajax go to server
+         url: '/restaurant',
+         method: 'GET'//reqst to server           
+     }).then(function (response) {
+         $('#ulOutput').empty();
+         response.forEach(function (restaurant) {
+             $('#ulOutput').append(`
+                <li id="liOutput">
+                    Name: ${restaurant.name}
+                    Type: ${restaurant.type}
+                                     
+                </li>
+                `)
+         })//end loop
+
+     })//end .then
+ }
--- server/server.js
+++ server/server.js
@@ -2,10 +2,37 @@ let express = require('express');
 let bodyParser = require('body-parser')// goes with req.body ..DO NOT FORGET
 let PORT = 5000;
 let app = express();
+const pg = require('pg');
 app.use(express.static('server/public'));// get any reuqst, static
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const pool = pg.Pool({//connects to postgres
+    host: 'localhost',//where is the database
+    port: 5432,
+    database: 'restaurant',// name of database
+    max: 10, //number of connections at 1 time
+    idleTimeoutMillis: 30000 // 30s to try to connect, othewise cancel query
+});// end pg.pool
 
+pool.on('connect', () => {
+    console.log('postgres connected');
+});// end pool connect
+
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error);
+})// end pool error
+
+app.get('/restaurant', (req, res) => {// get info from server
+    pool.query('SELECT * FROM "restaurant" ')//relation error is table in teminal
+
+        .then((result) => {
+            res.send(result.rows);
+            console.log(result.rows);// each object is going to be a row in from music_library
+        }).catch((error) => {
+            console.log('error with books select GET', error);
+            res.sendStatus(500);
+        });
+});//end app.get
 
 
 
